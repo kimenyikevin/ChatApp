a1f799e768b310c6a121b916a80f01d49a4abcc1
kimenyi kevin <kimenyikevin@gmail.com>
2019-12-20 10:36:37 +0000

    feature(edit server): modify server [finishes: ....]

--- server/server.js
+++ server/server.js
@@ -3,32 +3,17 @@ import config from 'dotenv';
 import socket from 'socket.io';
 import http from 'http';
 import cors from 'cors';
-import path from 'path';
 import bodyParser from 'body-parser';
 import users from './routers/user';
-import webpack, { Compiler } from 'webpack';
-import webpackMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware  from 'webpack-hot-middleware';
-// import webpackConfig from  '../webpack.config';
 import  {  addUser, removeUser, getUser, getUsersInRoom  } from './socketUser/User';
 import './models/user';
 
 config.config();
 const port = process.env.PORT || 3000;
 const app = express();
-// const compiler = webpack(webpackConfig);
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(cors());
-// app.use(webpackMiddleware(compiler));
-app.use(express.static(path.join(__dirname, 'build')))
-// app.use(webpackHotMiddleware(compiler, {
-//     hot: true,
-//     publicPath: webpackConfig.output.publicPath,
-//     noInfo: true
-// }));
-
-
 
 const server = http.createServer(app);
 const io = socket(server);
@@ -60,15 +45,7 @@ io.on('connection', (socket) =>{
         }
     })
 })
-app.use(  (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 app.use('/api/v1', users);
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'build', '../public/index.html'))
-})
 server.listen(port, () =>  console.log('app running on port ', port))
 
 export default app;
